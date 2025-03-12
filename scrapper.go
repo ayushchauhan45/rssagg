7e9e0c0348dc7582a467c9331c7438d8317a3772
ayushchauhan45 <chauhanasc2004@gmail.com>
2025-03-12 23:32:37 +0530

    Add post to db

--- scrapper.go
+++ scrapper.go
@@ -2,59 +2,81 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
-	"time"
 	"sync"
+	"time"
 
 	"github.com/ayushchauhan_45/rssagg/internal/database"
+	"github.com/google/uuid"
 )
-
-
 
 func startScrapping(
 	db *database.Queries,
-	concurrency int, 
+	concurrency int,
 	timeBetweenRequest time.Duration,
-){
+) {
 	log.Printf("Scrapping on %v goroutines every %s duration", concurrency, timeBetweenRequest)
-    
-	ticker:= time.NewTicker(timeBetweenRequest)
-	
+
+	ticker := time.NewTicker(timeBetweenRequest)
+
 	for ; ; <-ticker.C {
-		feeds,err:= db.GetNextFeedsToFetch(
+		feeds, err := db.GetNextFeedsToFetch(
 			context.Background(),
 			int32(concurrency),
 		)
-		if err!=nil{
-			log.Println("Error fetching feeds:",err)
+		if err != nil {
+			log.Println("Error fetching feeds:", err)
 			continue
 		}
 		wg := &sync.WaitGroup{}
-		for _,feed := range feeds{
+		for _, feed := range feeds {
 			wg.Add(1)
 
-			go srcapFeed(db,wg,feed)
+			go srcapFeed(db, wg, feed)
 		}
-        wg.Wait()
+		wg.Wait()
 	}
 
 }
 
-func srcapFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed){
+func srcapFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	defer wg.Done()
-	_,err:= db.MarkFeedAsFetched(context.Background(),feed.ID)
+	_, err := db.MarkFeedAsFetched(context.Background(), feed.ID)
 
-	if err!=nil{
-		log.Println("Error marking feed as fetched:",err)
+	if err != nil {
+		log.Println("Error marking feed as fetched:", err)
 	}
-	rssFeed,err:= urlToFeed(feed.Url)
-	if err!=nil{
-		log.Println("Error fetching feed:",err)
+	rssFeed, err := urlToFeed(feed.Url)
+	if err != nil {
+		log.Println("Error fetching feed 2:", err)
 		return
 	}
 
-	for _, item := range rssFeed.Channel.Items{
-		log.Println("Found post",item, "on feed",feed.Name)
+	for _, item := range rssFeed.Channel.Items {
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("Error parsing date %v with err %w:", item.PubDate, err)
+			continue
+		}
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Descreption: description,
+			PublishedAt: pubAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			log.Println("Error creating post:", err)
+		}
 	}
-	log.Printf("Feed %s collected ,%v posts found",feed.Name,len(rssFeed.Channel.Items))
+	log.Printf("Feed %s collected ,%v posts found", feed.Name, len(rssFeed.Channel.Items))
 }
